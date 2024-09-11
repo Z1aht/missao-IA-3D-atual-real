097b77b8edf427c8548bab97b4e957ca8daa8b10
Z1aht <130074959+Z1aht@users.noreply.github.com>
2024-09-11 10:12:37 -0300

    Update aleatório.js

--- js/aleatório.js
+++ js/aleatório.js
@@ -1,4 +1,8 @@
+const nomes = ["Lily", "Nay", "Julia", "Marcos", "Phill", "Luan"];
+
 export function aleatório (lista)
   const posicao = Math.floor(Matha.random()* lista.lenght);
   return lista[posição];
 }
+
+export const nomes = aleatorio(nomes)
