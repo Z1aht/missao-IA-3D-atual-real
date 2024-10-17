f5e7a4d236e66f12b541d1aa7ca23de4b48d859e
Z1aht <130074959+Z1aht@users.noreply.github.com>
2024-10-16 22:39:06 -0300

    Delete js/aleatorio.js

--- js/aleatorio.js
+++
@@ -1,8 +0,0 @@
-const nomes = ["Lily", "Kelly", "João", "Carlo", "Julio", "Alex"];
-
-export function aleatorio (lista){
-    const posicao = Math.floor(Math.random()* lista.length);
-    return lista[posicao];
-}
-
-export const nome = aleatorio(nomes)
